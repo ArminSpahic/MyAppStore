749a7831d8a62909975ed11a419de084c18cb209
ArminSpahic <armin1spahic@gmail.com>
2018-09-25 15:39:50 +0200

    Initial Commit

---
+++ MyAppStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAppStore
+//
+//  Created by Armin Spahic on 25/09/2018.
+//  Copyright © 2018 Armin Spahic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
